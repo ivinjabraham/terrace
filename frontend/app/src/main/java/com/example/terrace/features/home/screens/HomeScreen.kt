852bdac3a8e8f3fa90cf79929d74df76c4d87624
satvshr <satvmishi@gmail.com>
2025-02-22 14:02:55 +0530

    Removed unecessary imports

--- frontend/app/src/main/java/com/example/terrace/features/home/screens/HomeScreen.kt
+++ frontend/app/src/main/java/com/example/terrace/features/home/screens/HomeScreen.kt
@@ -1,15 +1,11 @@
 package com.example.terrace.features.auth.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.terrace.BackgroundComponent
-import com.example.terrace.core.navigation.Screen
-import com.example.terrace.core.navigation.safeNavigate
 
 @Composable
 fun HomeScreen(navController: NavController) {
