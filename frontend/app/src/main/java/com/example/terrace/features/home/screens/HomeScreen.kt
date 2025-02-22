800e74b5f915fe8c07aed8096b69ede1640b029f
satvshr <satvmishi@gmail.com>
2025-02-23 04:14:29 +0530

    Added textbox to display constellation details

--- frontend/app/src/main/java/com/example/terrace/features/home/screens/HomeScreen.kt
+++ frontend/app/src/main/java/com/example/terrace/features/home/screens/HomeScreen.kt
@@ -37,6 +37,18 @@ import com.example.terrace.features.global.layout.screen.LayoutComponent
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.border
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.drawscope.withTransform
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
