262ed79e43a581639758f5ed98c39f3722f3a529
satvshr <satvmishi@gmail.com>
2025-02-22 14:02:18 +0530

    Removed unecessary imports

--- frontend/app/src/main/java/com/example/terrace/MainActivity.kt
+++ frontend/app/src/main/java/com/example/terrace/MainActivity.kt
@@ -5,15 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.example.terrace.core.navigation.NavigationGraph
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import com.example.terrace.features.auth.screens.HomeScreen
-import com.example.terrace.features.auth.screens.LoginScreen
 import android.graphics.Color
 import android.os.Build
 import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.ViewCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
