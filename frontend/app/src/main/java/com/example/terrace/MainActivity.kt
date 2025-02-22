e1e029af1a0bc70893a051c5915630a8df842566
satvshr <satvmishi@gmail.com>
2025-02-22 13:04:52 +0530

    Compatible PR

--- frontend/app/src/main/java/com/example/terrace/MainActivity.kt
+++ frontend/app/src/main/java/com/example/terrace/MainActivity.kt
@@ -8,11 +8,22 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.terrace.features.auth.screens.HomeScreen
 import com.example.terrace.features.auth.screens.LoginScreen
-
+import android.graphics.Color
+import android.os.Build
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.ViewCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        // Optional: make the status and navigation bars transparent.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = Color.TRANSPARENT
+            window.navigationBarColor = Color.TRANSPARENT
+        }
         setContent {
             val navController = rememberNavController()
             NavHost(navController, startDestination = "login") {
