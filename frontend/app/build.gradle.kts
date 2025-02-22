b1991ab4281aa20235ab826ea78e7faaf73434b1
Wreck <hemanthkrishnakumar@gmail.com>
2025-02-22 17:41:58 +0530

    add dynamic star background

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -65,5 +65,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("io.coil-kt:coil-compose:2.2.2")
+
 
 }
