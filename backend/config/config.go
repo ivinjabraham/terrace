5d4285c50ae2e1657fb154d21a237487cf4e96f2
Ivin Joel Abraham <ivinjabraham@gmail.com>
2025-02-23 00:18:30 +0530

    add dotenv support

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,23 +1,29 @@
 package config
 
 import (
-    "log"
-    "os"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 var (
-    DB_URI     string
-    JWT_SECRET []byte
+	DB_URI     string
+	JWT_SECRET []byte
 )
 
 func LoadConfig() {
-    DB_URI = os.Getenv("DB_URI")
-    if DB_URI == "" {
-        log.Fatal("DB_URI environment variable not set")
-    }
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found.")
+	}
 
-    JWT_SECRET = []byte(os.Getenv("JWT_SECRET"))
-    if len(JWT_SECRET) == 0 {
-        log.Fatal("JWT_SECRET environment variable not set")
-    }
+	DB_URI = os.Getenv("DB_URI")
+	if DB_URI == "" {
+		log.Fatal("DB_URI environment variable not set")
+	}
+
+	JWT_SECRET = []byte(os.Getenv("JWT_SECRET"))
+	if len(JWT_SECRET) == 0 {
+		log.Fatal("JWT_SECRET environment variable not set")
+	}
 }
